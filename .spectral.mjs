c93b2fbdf0040251643fcfd2eab3fcbea8dbc75e
hwangdonggeun70@gmail.com <hwangdonggeun70@gmail.com>
2026-04-08 22:05:00 +0900

    Added .spectral.mjs

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjMwOTc3MQ/spectral.js?branch_slug=master&token=142b2e2c-6f0c-4abb-a48e-7d355ec72b94";
+export default { extends: ruleset };
