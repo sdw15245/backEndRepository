22515d93bbdfdc7a011f363fa52c45fc0592cb2f
sdw <sdw0098@gmail.com>
2026-04-23 13:18:57 +0900

    schema, 기본값 or 이미 있는값2

--- src/main/java/com/sweep/project/alarm/dto/AlarmCreateRequest.java
+++ src/main/java/com/sweep/project/alarm/dto/AlarmCreateRequest.java
@@ -28,12 +28,12 @@ public record AlarmCreateRequest(
         @NotNull
         LocalDateTime startTime,
 
-        @Schema(description = "출발 전 준비 시간 (분)", example = "60", requiredMode = Schema.RequiredMode.REQUIRED)
+        @Schema(description = "출발 전 준비 시간 (분), 기본값 60", example = "60", requiredMode = Schema.RequiredMode.REQUIRED)
         @NotNull
         @Min(1) @Max(1440)
         Integer prepareTime,
 
-        @Schema(description = "준비 알람 발송 간격 (분)", example = "20", requiredMode = Schema.RequiredMode.REQUIRED)
+        @Schema(description = "준비 알람 발송 간격 (분), 기본값 20", example = "20", requiredMode = Schema.RequiredMode.REQUIRED)
         @NotNull
         @Min(1) @Max(240)
         Integer interval,
