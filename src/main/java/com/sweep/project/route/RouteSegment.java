e79a3fcf1947a60b6eb237969eae3e00d93d42d1
dong3058 <hwangdonggeun70@gmail.com>
2026-04-27 14:59:26 +0900

    update swagger

--- src/main/java/com/sweep/project/route/RouteSegment.java
+++ src/main/java/com/sweep/project/route/RouteSegment.java
@@ -1,9 +1,24 @@
 package com.sweep.project.route;
 
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.sweep.project.route.bus.BusRoute;
+import com.sweep.project.route.mixed.MixedRoute;
+import com.sweep.project.route.subway.SubwayRoute;
+import io.swagger.v3.oas.annotations.media.Schema;
+
 /**
  * 경로 구간 공통 인터페이스
  * trafficType: 1 = 지하철, 2 = 버스, 3 = 도보
  */
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, property = "@type")
+@JsonSubTypes({
+        @JsonSubTypes.Type(value = SubwayRoute.class, name = "subway"),
+        @JsonSubTypes.Type(value = BusRoute.class,    name = "bus"),
+        @JsonSubTypes.Type(value = MixedRoute.class,  name = "mixed")
+})
+@Schema(oneOf = {SubwayRoute.class, BusRoute.class, MixedRoute.class},
+        discriminatorProperty = "@type")
 public interface RouteSegment {
     int getTrafficType();
     int getSectionTime();
