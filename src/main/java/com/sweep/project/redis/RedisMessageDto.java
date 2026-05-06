37fefcb110624b2c8762851cbaa473388f7e6276
sdw <sdw0098@gmail.com>
2026-05-07 02:57:50 +0900

    알림 제목/본문 및 발송 로그 문구 반영

--- src/main/java/com/sweep/project/redis/RedisMessageDto.java
+++ src/main/java/com/sweep/project/redis/RedisMessageDto.java
@@ -13,11 +13,24 @@ public class RedisMessageDto {
     private String alarmId;
     private String token;
     private String alarmType;
+    private Integer remainingMinutes;       // ex) remainingMinutes == 20이라면 "20분 후에 출발해야 해요!"
+    private Boolean prepareStart;           // ex) prepareStart == true 라면 "지금 준비 시작해야 해요!"
 
     public RedisMessageDto(String memberId, String alarmId,String alarmType, String token) {
+        this(memberId, alarmId, alarmType, token, null, false);
+    }
+
+    public RedisMessageDto(String memberId, String alarmId,String alarmType, String token, Integer remainingMinutes) {
+        this(memberId, alarmId, alarmType, token, remainingMinutes, false);
+    }
+
+    public RedisMessageDto(String memberId, String alarmId,String alarmType, String token,
+                           Integer remainingMinutes, Boolean prepareStart) {
         this.memberId = memberId;
         this.alarmId = alarmId;
         this.token = token;
         this.alarmType = alarmType;
+        this.remainingMinutes = remainingMinutes;
+        this.prepareStart = prepareStart;
     }
 }
