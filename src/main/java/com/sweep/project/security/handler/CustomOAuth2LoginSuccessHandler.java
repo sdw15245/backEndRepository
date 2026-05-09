246063dd22043011144fd507b6a8b56e0024d9fa
dong3058 <140478288+dong3058@users.noreply.github.com>
2026-05-09 12:50:38 +0900

    Update redirect URL for OAuth2 login success

--- src/main/java/com/sweep/project/security/handler/CustomOAuth2LoginSuccessHandler.java
+++ src/main/java/com/sweep/project/security/handler/CustomOAuth2LoginSuccessHandler.java
@@ -40,8 +40,8 @@ public class CustomOAuth2LoginSuccessHandler extends SimpleUrlAuthenticationSucc
             redisUserInfoService.setLoginUserInfo(customOAuth2User.getId(), member, refreshToken);
             //response.addHeader(AUTHORIZATION, TOKEN_PREFIX.getValue() + accessToken);
 
-            response.sendRedirect("http://localhost:5173/oauth2/callback?token="+accessToken);
-            //response.sendRedirect("https://hodadak.vercel.app/oauth2/callback?token=" + accessToken);
+            //response.sendRedirect("http://localhost:5173/oauth2/callback?token="+accessToken);
+            response.sendRedirect("https://hodadak.vercel.app/oauth2/callback?token=" + accessToken);
             log.info("{} 유저에대한 로그인이 정상적으로 되었습니다", customOAuth2User.getEmail());
         }
         catch (Exception e){
