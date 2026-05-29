ab452e7edb413fe267a2d5f55a7df3f112378813
minjoon1015 <3283alswns@naver.com>
2026-05-30 00:51:10 +0900

    feat: allow nullable placeName column

--- src/main/java/com/sweep/project/favoritelocation/domain/FavoriteLocation.java
+++ src/main/java/com/sweep/project/favoritelocation/domain/FavoriteLocation.java
@@ -26,7 +26,7 @@ public class FavoriteLocation {
     @Column(nullable = false, length = 50)
     private String name;
 
-    @Column(nullable = false, length = 100)
+    @Column(length = 100)
     private String placeName;
 
     @Column(nullable = false, length = 255)
